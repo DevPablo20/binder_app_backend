9450c1c9c2b20b51996da58b988969209cd62e3d
DevPablo20 <jaberpablo@gmail.com>
2026-06-16 08:47:09 -0300

    fix: added patch to CORS

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: corsOrigin,
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
